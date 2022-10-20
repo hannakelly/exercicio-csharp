d4ab16b2f9f64f7129bcdf1aa4c4da2c961a4df6
hannakelly <hanna.kellyfc@gmail.com>
2022-10-19 22:00:06 -0300

    primeiro-csharp

---
+++ Csharp/Namespace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Text;
+
+namespace  Primeiro
+{
+    class Classe
+    {
+
+    }
+
+}
+
+namespace Segundo
+{
+    class Classe
+    {
+
+    }
+}
---
+++ Csharp/interface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IAnimal {
+        void Nome(string nome);
+
+        void Dono(string nomeDono);
+
+        void Especie(string especie);
+          }
+
+    class Animal : IAnimal
+    {
+       public string Nome { get; set; }
+
+       public string nomeDono { get; set; }
+
+       public string especie { get; set; }
+
+
+       void IAnimal.Nome(string nome)
+        {
+            this.Nome = nome;
+        }
+        void IAnimal.Dono(string nomeDono)
+        {
+            this.nomeDono = nomeDono;
+        }
+
+        void IAnimal.Especie(string especie)
+        {
+            this.Especie = especie;
+        }
+        
+
+
+    }
+}
